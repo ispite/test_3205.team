ef6e3d2990872131ee41243d151ffb32b8c144ef
Denis Solovyov <infinityx93@gmail.com>
2022-11-14 22:12:34 +0300

    test kotlinter 2

--- app/src/main/java/ru/skillbox/test_3205team/MainActivity.kt
+++ app/src/main/java/ru/skillbox/test_3205team/MainActivity.kt
@@ -1,17 +1,11 @@
 package ru.skillbox.test_3205team
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-
-
-
-
-
     }
 }
--- app/src/test/java/ru/skillbox/test_3205team/ExampleUnitTest.kt
+++ app/src/test/java/ru/skillbox/test_3205team/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package ru.skillbox.test_3205team
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
