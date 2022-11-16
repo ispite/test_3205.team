b9a50b3e4c2afa95594fb053dbeead0b051d760a
Denis Solovyov <infinityx93@gmail.com>
2022-11-16 21:37:59 +0300

    added splash screen

--- app/src/main/java/ru/skillbox/test_3205team/MainActivity.kt
+++ app/src/main/java/ru/skillbox/test_3205team/MainActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setTheme(R.style.Theme_Test_3205team)
         setContentView(R.layout.activity_main)
     }
 }
