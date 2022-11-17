b2c1ae33815a4d0c14c3cb13185068b3dca5be23
Denis Solovyov <infinityx93@gmail.com>
2022-11-17 22:05:58 +0300

    added ReposFragment.kt and reworked MainFragment.kt

--- app/src/main/java/ru/skillbox/test_3205team/MainFragment.kt
+++ app/src/main/java/ru/skillbox/test_3205team/MainFragment.kt
@@ -7,4 +7,6 @@ import ru.skillbox.test_3205team.databinding.FragmentMainBinding
 class MainFragment : Fragment(R.layout.fragment_main) {
 
     private val binding: FragmentMainBinding by viewBinding(FragmentMainBinding::bind)
+
+    // TODO почитать gitHub API нужна ли авторизация для доступа к репозиториям и имплементировать вот это всё
 }
--- app/src/main/java/ru/skillbox/test_3205team/ReposFragment.kt
+++ app/src/main/java/ru/skillbox/test_3205team/ReposFragment.kt
@@ -2,4 +2,5 @@ package ru.skillbox.test_3205team
 
 import androidx.fragment.app.Fragment
 
-class SplashFragment : Fragment()
+class ReposFragment : Fragment(R.layout.fragment_repos) {
+}
